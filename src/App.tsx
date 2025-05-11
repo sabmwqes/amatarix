d2842f01e6b15418e27546420a6687256a42f02a
sabmwqes <91858685+sabmwqes@users.noreply.github.com>
2025-05-11 22:47:29 +0900

    [add] initialize project with React + Typescript + Vite

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>
+        Amatar<span className="red">ix</span>
+      </h1>
+      <h3>Under construction</h3>
+      <img src={reactLogo} className="logo" alt="React logo" />
+    </>
+  )
+}
+
+export default App
